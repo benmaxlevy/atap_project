73de0074d0aad5b5b6b7826d7585ebe793012b20
Ben <general@simplexwebsites.com>
2022-05-16 21:56:43 -0400

    Make .env.example, work on expand_dataset

--- data/expand_dataset.py
+++ data/expand_dataset.py
@@ -1,9 +1,22 @@
 if __name__ == "__main__":
     import pandas as pd
     import requests
-    dataset = pd.read_csv("data/dataset.csv", names = ["date", "closures"], skiprows = [0])
+    import os
+    import time
+    import datetime
+    from dotenv import load_dotenv
+    load_dotenv("../.env")
+    df = pd.read_csv("dataset.csv", names=["date", "closures"], skiprows=[0])
     for r in df.iterrows():
-        # fetch weather data for the day
-        # first, get /points/{points}/stations
+        # fetch weather data for the day from https://api.stormglass.io/v2/weather/point
+        # convert the date to UNIX timestamp
+        # create python timestamp from r[1]["date"]
+        date = datetime.datetime.strptime(r[1]["date"], "%m/%d/%Y")
+        unix_timestamp = int(time.mktime(date.timetuple()))
+        # get lat and lon from env
+        lat = os.getenv("LAT")
+        lon = os.getenv("LON")
+        # create the request
+        url = "https://api.stormglass.io/v2/weather/point?lat={lat}&lng={lon}&start={unix_timestamp}&end={unix_timestamp}"
 
 
